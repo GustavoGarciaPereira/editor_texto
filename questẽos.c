86743e4ae2a70386c816beee915e1ce2ea5af880
Gustavo Garcia Pereira <gusgurtavo@gmail.com>
2020-10-14 23:49:15 -0300

    Delete questẽos.c

--- questẽos.c
+++
@@ -1,7 +0,0 @@
-1) O que é a Ciência da Computação?
-    -prcessos logicos-metematicos de manipulação de representação lingustica
-2) Quais são os fundamentos da Ciência da Computação?
-    -fundamenta em três pilares "lógica","Matemática" e "Linguagem"|sendo ela formal ou não. 
-3) Especificamente, identifique as contribuições da área de IHC para os Fundamentosda Ciência da Computação e as caracterize.
-    - 
-4) Como você percebe a integração da área de IHC com os Fundamentos da Ciênciada Computação?
